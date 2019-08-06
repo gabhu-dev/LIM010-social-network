646881611c3575f913015bccc2b3a35497914c3d
gabhu <gabhu@hotmail.es>
2019-08-06 12:10:06 -0500

    primer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-// Este es el punto de entrada de tu aplicacion
-
-import { myFunction } from './lib/index.js';
-
-myFunction();
+// login es login
+const provider = new firebase.auth.GoogleAuthProvider();
+$('#login').click(() => {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+ 
