d48f4b6c4c120350be3a63875e1538917569b579
SarAbigail <samihimezu@gmail.com>
2019-08-19 10:37:28 -0500

    agregando estilo al input post

--- src/lib/views/profile.js
+++ src/lib/views/profile.js
@@ -2,7 +2,7 @@ export default () => {
   const viewProfile = document.createElement('div');
   const template = `
   <header>
-    <nav class="flex-r center-items">
+    <nav class="flex-r center-items bg-color-blue h-f-height">
       <p>Nombre de Usuario</p>
       <p>Logo</p>
       <p>Cerrar Sesión</p>
@@ -13,15 +13,18 @@ export default () => {
       aqui va el perfil del usuario
     </div>
     <div class="two-col center-items">
-      <input type="text" id="post" class="post">
+      <label class="post-l bg-color-pink">
+        <input type="text" id="post" class="post" placeholder="¿Qué quieres compartir?">
+        <button type="button" class="w-max bg-color-blue btn-share c-darkblue">Compartir</button>
+      </label>
       <button type="button" id="savePost">Postear</button>
     </div>
   </div>
-  <footer>
+  <footer  class="bg-color-pink h-f-height">
     <p class="flex-r center-items">aqui va el footer</p>
   </footer>`;
   viewProfile.innerHTML = template;
   viewProfile.setAttribute('class', 'size');
-  console.log(document.getElementById('add-post'));
+  console.log(document.getElementById('post'));
   return viewProfile;
 };
