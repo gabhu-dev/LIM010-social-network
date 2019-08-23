d7c1a769b83f3dac493f4eee1d42d706484e963e
gabhu-dev <gabhu@hotmail.es>
2019-08-22 21:44:46 -0500

    menu con estilos y maquetacion de home

--- src/lib/views/home.js
+++ src/lib/views/home.js
@@ -6,35 +6,39 @@ export default () => {
   const template = `
   <!-- ------------------------------------------------ -->
   <header class="bg-color-blue ">
-  <input type="checkbox" id="btn-menu" >
-  <!--<img src="https://raw.githubusercontent.com/gabhu-dev/LIM010-social-network/master/src/lib/img/logo3.png"/ class="logo-home" alt="logo hey! hallyu">-->
+  <input type="checkbox" id="btn-menu" class="hide" >
   <label for="btn-menu"><i class='bx bx-menu bx-flip-vertical nav-icon' ></i></label>
+    <img src="https://raw.githubusercontent.com/gabhu-dev/LIM010-social-network/master/src/lib/img/logo3.png"/ class="logo-home" alt="logo hey! hallyu">
     <nav class="nav">
-      <ul>
+      <ul class="flex-c">
         <li><a href="">usuario</a></li>
-        <li><a href="">alguno</a></li>
+        <li><a href=""> <img src="https://raw.githubusercontent.com/gabhu-dev/LIM010-social-network/master/src/lib/img/logo3.png"/ class="logo-nav" alt="logo hey! hallyu"></a></li>
         <li type="button" id="log-out"><a href="">cerrar sesion</a></li>
       </ul>
     </nav>
   </header>
   <!-- ------------------------------------------------ -->
-<div class="flex-r just-cont-cen">
-    <div class="center-items flex-cn size-profile">
-      <label class="img-profile"><p>Imagen de perfil</p></label>
-      <p>Nombre del usuario</p>
+<div class="flex-c center-items">
+<div class="center-items flex-r size-profile">
+
+  <div class="img-profile"></div>
+  <div>Nombre del usuario</div>
+</div>
+<div class="two-col center-items">
+  <label class="post-label bg-color-pink">
+  
+    <input type="text" id="text-post" class="post flex-c c-darkblue" placeholder="¿Qué quieres compartir?">
+    <button type="button" id="btn-img"><i class='bx bxs-image'></i></button>
+    <button type="button" id="btn-save" class="bg-color-blue btn-share c-darkblue">Guardar</button>
+    <button type="button" id="btn-up">subir</button>
+  </label>
+  <label class="flex-c post-label bg-color-pink"> 
+  <p> nombre del que publico</p>
+    <div id="post-up" class=" flex-c c-darkblue">
+      <td>Celda 1</td> 
     </div>
-    <div class="two-col center-items flex-c">
-      <label class="flex-c post-label bg-color-pink">
-        <input type="text" id="text-post" class="post flex-c c-darkblue" placeholder="¿Qué quieres compartir?">
-        <button type="button" id="btn-save" class="w-h-max bg-color-blue btn-share c-darkblue flex-c">Guardar</button>
-        <button type="button" id="btn-up">subir</button>
-      </label>
-      <label class="flex-c post-label bg-color-pink">
-        <div id="post-up" class="post flex-c c-darkblue">
-          <td>Celda 1</td> 
-        </div>
-      </label>
-    </div>
+  </label>
+</div>
 </div> 
   <footer class="flex-r bg-color-pink h-f-height center-items">
     <p class="w-h-max">Creado por Gabhu y Sara con <3. Todos los Derechos Reservados.</p>
