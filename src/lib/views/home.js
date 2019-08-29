68b338b2ee3e13166f562ca5b987fc872b7a959a
SarAbigail <samihimezu@gmail.com>
2019-08-28 21:16:29 -0500

    Probando editar post

--- src/lib/views/home.js
+++ src/lib/views/home.js
@@ -1,13 +1,11 @@
+/* eslint-disable no-plusplus */
 import { obtainProfile } from '../controller/users-profile.js';
 import { logOut } from '../controller/sign-in-control.js';
 import { currentUser } from '../model/firebase-auth.js';
-// import { itemPost } from './post.js';
-// import { profileUser } from '../controller/user-info-control.js';
-// import { logOut } from '../controller/sign-in-control.js';
-import { save, readPost } from '../controller/home-control.js';
-// import { deleteData } from '../controller/post-data.js';
+import { listPosts } from './view-post.js';
+import { functionSharePost } from '../controller/post-controll.js';
 
-export default () => {
+export default (allPost) => {
   const viewHome = document.createElement('div');
   const template = `
   <!-- --------------------header---------------------------- -->
@@ -24,52 +22,47 @@ export default () => {
     </nav>
   </header>
   <!-- ------------------------------------------------ -->
- <div class="banner-post flex-c">
-   <div class="center-items flex-r size-profile">
-     <div id="img-profile" class="img-profile">name</div>
-     <div>
-      <p id="user-info"></p>
-     </div>
-   </div>
-   <div class="divpost">
-     <label class="post-label bg-color-pink">
-       <input type="text" id="text-post" class="post flex-c c-darkblue" placeholder="¿Qué quieres compartir?">
-       <div class="buttons-post-label">
-       <button type="button" id="btn-img"><i class='bx bxs-image'></i></button>
-       <button type="button" id="btn-save" class="bg-color-blue btn-share c-darkblue">Guardar</button>
-       <button type="button" id="btn-up">subir</button>
-       </div>
-     </label>
-         <div id="post-up" class=" post-up flex-c c-darkblue bg-color-pink">
-      
-         </div>
-       <!-- <div class="options-like-deleted">
-         <button><i class='bx bx-heart'></i></button>
-         <button><i class='bx bxl-telegram'></i></button>
-       </div> -->
-      </label> 
+  <div class="banner-post flex-c center-items">
+    <div class="center-items flex-r size-profile">
+      <div id="img-profile" class="img-profile"></div>
+      <div>
+        <p id="user-info"></p>
+      </div>
     </div>
-   </div> 
+    <div class="two-col center-items">
+      <div class="post-label bg-color-pink">
+        <textarea cols="50" rows="2" type="text" id="text-post" class="post flex-c c-darkblue" placeholder="¿Qué quieres compartir?"></textarea>
+        <select id="mode">
+          <option value="Público">Público</option>
+          <option value="Privado">Privado</option>
+        </select>
+        <button type="button" id="btn-img"><i class='bx bxs-image'></i></button>
+        <button type="button" id="btn-save" class="bg-color-blue btn-share c-darkblue">Compartir</button>
+      </div>
+    </div>
+    <div class="form-post" id="container-list-post"></div>
+  </div> 
    <!-- -------------------------------------------------- -->
   <footer class="flex-r bg-color-pink h-f-height center-items">
     <p class="w-h-max">Creado por Gabhu y Sara con <3. Todos los Derechos Reservados.</p>
   </footer>`;
   viewHome.innerHTML = template;
   viewHome.setAttribute('class', 'size flex-c ');
-  const btnLogOut = viewHome.querySelector('#log-out');
-  btnLogOut.addEventListener('click', logOut);
 
+  const btnSharePost = viewHome.querySelector('#btn-save');
+  const containerListPost = viewHome.querySelector('#container-list-post');
+  const btnLogOut = viewHome.querySelector('#log-out');
+
+  // Publicar post
+  btnSharePost.addEventListener('click', functionSharePost);
+  // Imprimir todo las publicaciones dinamicamente
+  for (let i = 0; i < allPost.length; i++) {
+    containerListPost.appendChild(listPosts(allPost[i]));
+  }
+  // Cerrar sesión
+  btnLogOut.addEventListener('click', logOut);
+  // Da el usuario actual y muestra sus datos al loguearse
   const user = currentUser();
   obtainProfile(user.uid);
-
-  const btnSave = viewHome.querySelector('#btn-save');
-  btnSave.addEventListener('click', save);
-  const btnUp = viewHome.querySelector('#btn-up');
-  btnUp.addEventListener('click', readPost);
-  // const label = viewHome.querySelector('label-publicate');
-  // posts.forEach((post) => {
-  //   label.appendChild(itemPost(post));
-  // });
-
   return viewHome;
 };
